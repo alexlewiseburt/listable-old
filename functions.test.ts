213951266a594a6664411290d413c21a0e28a185
Alexandrea Burt <alexlewiseburt@gmail.com>
2022-11-10 00:20:54 -0500

    unit tests

--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,10 @@
 const {shuffleArray} = require('./utils')
 
 describe('shuffleArray should', () => {
-    // CODE HERE
+    test("return an array", () => {
+        expect(shuffleArray([])).toEqual([])
+    })
+    test("items are shuffled", () => {
+        expect(shuffleArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])).not.toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    })
 })
